5b525e9f9a0a97ebaa2096f02fa7f4d6a8668485
The_Number_e <edwardx999@gmail.com>
2020-02-14 20:31:07 -0500

    proper ; parsing

--- addme.ps1
+++ addme.ps1
@@ -2,17 +2,95 @@ param (
 	[Parameter(Position=0)][String]$Path = ".",
 	[Parameter(Position=1)][String]$Target = "User"
 )
+
+Function PathSplit([string]$path)
+{
+	$len = $path.Length
+	$ret = New-Object System.Collections.ArrayList
+	if($len -eq 0)
+	{
+		return ret
+	}
+	$in_quotes = $False
+	$start = 0
+	for($i = 0; $i -lt $len; $i += 1)
+	{
+		$char = $path[$i]
+		if($in_quotes)
+		{
+			if($char -eq [char]'"')
+			{
+				$in_quotes = $False
+				if($start -lt $i)
+				{
+					$ret += $path.Substring($start, $i - $start)
+				}
+				$next = $i + 1
+				if($next -lt $len)
+				{
+					if($path[$next] -ne [char]';')
+					{
+						throw "Invalid characters after quoted path"
+					}
+					$start = $i + 2
+				}
+				else
+				{
+					$start = $len
+					break
+				}
+			}
+		}
+		else
+		{
+			if($char -eq [char]'"')
+			{
+				if($start -eq $i)
+				{
+					$in_quotes = $True
+					$start = $i + 1
+				}
+				else
+				{
+					throw "Invalid characters before quoted path"
+				}
+			}
+			if($char -eq [char]';')
+			{
+				if($start -lt $i)
+				{
+					$ret += $path.Substring($start, $i - $start)
+				}
+				$start = $i + 1
+			}
+		}
+	}
+	if($in_quotes)
+	{
+		throw "Path has unclosed quotes"
+	}
+	if($start -lt $len)
+	{
+		$ret += $path.Substring($start, $len - $start)
+	}
+	return $ret
+}
+
 try
 {
 	$current_path = [Environment]::GetEnvironmentVariables($Target).Path
 	$current_dir = (Resolve-Path -LiteralPath $Path).ToString()
-	$paths = $current_path.Split(";") # Not proper parsing but good enough
+	$paths = PathSplit($current_path)
 	foreach($path in $paths)
 	{
 		if($path -eq $current_dir)
 		{
 			exit
 		}
+	}
+	if($current_dir.Contains(";"))
+	{
+		$current_dir = "`"${current_dir}`""
 	}
 	if($paths.Count -eq 0)
 	{
