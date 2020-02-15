a193cc295e552d167ae2d8975c254e39f3102245
The_Number_e <edwardx999@gmail.com>
2020-02-14 20:31:20 -0500

    ps1 profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,26 @@
+$_pmake_cores_found = $false 
+Function pmake
+{
+	if ($global:_pmake_cores_found -eq $false)
+	{
+		try
+		{
+			$global:_pmake_cores_found = (Get-WmiObject -class Win32_ComputerSystem).numberoflogicalprocessors
+			if(($global:_pmake_cores_found -eq $null) -or ($global:_pmake_cores_found -lt 1))
+			{
+				$global:_pmake_cores_found = 1
+			}
+		}
+		catch
+		{
+			$global:_pmake_cores_found = 1
+		}
+	}
+	make -j $global:_pmake_cores_found
+}
+
+Function mkcd($path)
+{
+	$null = New-Item $path -ItemType Directory -ea 0
+	cd $path
+}
