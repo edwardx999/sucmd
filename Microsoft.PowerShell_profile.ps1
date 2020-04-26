63b9580076f452b1b135774583e00d1b922e28f6
The_Number_e <edwardx999@gmail.com>
2020-04-25 21:48:20 -0400

    linkcopy and SleepComputer

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,9 @@
 $_pmake_cores_found = $false
 
+<#
+.Description
+make with -j set to available cores
+#>
 Function pmake
 {
 	if ($global:_pmake_cores_found -eq $false)
@@ -20,6 +24,10 @@ Function pmake
 	make -j $global:_pmake_cores_found
 }
 
+<#
+.Description
+mkdir and cd into that path
+#>
 Function mkcd($path)
 {
 	$null = New-Item $path -ItemType Directory -ea 0
@@ -43,7 +51,57 @@ Function mklink
 	cmd /c $cmd
 }
 
+<#
+.Description
+Make a "copy" of (a) file(s) into the current directory, given a filename (pattern) and optional link type.
+.Example
+linkcopy ../OtherFiles/*.mp4 /h
+#>
+Function linkcopy
+{
+	if($args.Count -gt 2)
+	{
+		throw "Too many arguments"
+	}
+	elseif($args.Count -lt 1)
+	{
+		throw "Too few arguments"
+	}
+	elseif($args.Count -eq 2)
+	{
+		if($args[0].StartsWith("/"))
+		{
+			$link_type = $args[0]
+			$path = $args[1]
+		}
+		elseif ($args[1].StartsWith("/"))
+		{
+			$link_type = $args[1]
+			$path = $args[0]
+		}
+		else
+		{
+			throw "Invalid Arguments"
+		}
+	}
+	else
+	{
+		$path = args[0]
+		$link_type = "/s"
+	}
+	$paths = Resolve-Path -Path $path
+	foreach($p in $paths)
+	{
+		mklink (Split-Path $p -Leaf) $p.ToString() $link_type
+	}
+}
+
 Function CommandLocation($command)
 {
 	return Split-Path -LiteralPath (Get-Command $command).Source
+}
+
+Function SleepComputer
+{
+	[System.Windows.Forms.Application]::SetSuspendState([System.Windows.Forms.PowerState]::Suspend, $false, $false)
 }
